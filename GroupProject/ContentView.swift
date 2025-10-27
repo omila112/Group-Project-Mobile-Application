1394b7f910af814818c16a9b23c322ee6f1b63bc
omila112 <omiladesilva12@gmail.com>
2025-10-27 11:23:11 +0530

    code
    code commits

---
+++ GroupProject/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  GroupProject
+//
+//  Created by Omila De Silva on 2025-10-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+// checking code commits 
---
+++ GroupProject/GroupProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupProjectApp.swift
+//  GroupProject
+//
+//  Created by Omila De Silva on 2025-10-27.
+//
+
+import SwiftUI
+
+@main
+struct GroupProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
