efa629fe1add0776e452b247b503baf976c75c7d
omila112 <omiladesilva12@gmail.com>
2025-10-28 16:26:08 +0530

    Tab VIew

--- GroupProject/ContentView.swift
+++ GroupProject/ContentView.swift
@@ -9,11 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            Tab("home", systemImage: "house") {
+                Text("Hello")
+            }
+            Tab("Favourites", systemImage: "star") {
+                Text("Fav")
+            }
         }
         .padding()
     }
